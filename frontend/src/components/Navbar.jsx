94a88e3a5c083258aacf0326c8b4a06770f5e64c
Nemoto Mailon <minatokiroisen77@gmail.com>
2025-11-26 12:35:22 +0900

    start create add note page

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,4 +1,4 @@
-import { Link } from 'react-router'
+import { Link } from 'react-router-dom'
 import { PlusIcon } from 'lucide-react'
 
 const Navbar = () => {
@@ -7,8 +7,8 @@ const Navbar = () => {
           <div className="mx-auto max-w-6xl p-4">
               <div className="flex items-center justify-between">
                   <h1 className="text-3xl font-bold text-primary font-mono tracking-tight">ThinkBoard</h1>
-                  <div className="flex items-center gap-4">
-                      <Link to={"/create"} className='btn btn-primary'>
+                  <div className="flex items-center gap-4 cursor-pointer">
+                      <Link to="/create" className='btn btn-primary'>
                           <PlusIcon className="size-5" />
                           <span>New Note</span>
                       </Link>
--- frontend/src/components/NoteCard.jsx
+++ frontend/src/components/NoteCard.jsx
@@ -1,5 +1,5 @@
 import { PenSquareIcon, Trash2Icon } from "lucide-react"
-import { Link } from "react-router"
+import { Link } from "react-router-dom"
 import { formatDate } from "../libs/utils"
 
 
